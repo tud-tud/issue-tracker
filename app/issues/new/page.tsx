0127a3d18a6d6af1a2816165e3dfe7646262e429
tud-tud <pornsudayompan@gmail.com>
2024-01-28 20:21:33 +0700

    Add a markdoown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextField } from "@radix-ui/themes";
 import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
   return (
@@ -9,7 +11,7 @@ const NewIssuePage = () => {
       <TextField.Root>
         <TextField.Input placeholder="Title" />
       </TextField.Root>
-      <TextArea placeholder="Deacription..."/>
+      <SimpleMDE placeholder="Deacription..." />
       <Button>Submit New Issue</Button>
     </div>
   );
